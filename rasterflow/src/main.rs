e25498f855e85db0d113ab2e24d310f27c250d17
2wei6 <2wei6@tuta.io>
2021-01-28 16:26:34 +0100

    added initialization depending on a set number of rows and cells.

--- rasterflow/src/main.rs
+++ rasterflow/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    const NROWS: usize = 4;
-    const NCOLS: usize = 4;
+    const NROWS: usize = 20;
+    const NCOLS: usize =20;
     const CELLSIZE: i32 = 1;
     const G: f32 = 9.81;
     
@@ -9,28 +9,47 @@ fn main() {
     const DT_OUT: f32 = 1.0;
     const DT_END: f32 = 60.0;
 
+    let depth_0 = 0.1;
+    let kst_default = 10.;
+
     // Digital Elevation Model in meters
-    let terrain = [[1.0, 1.2, 1.1, 1.0], 
-                   [1.4, 1.6, 1.3, 1.1], 
-                   [1.1, 1.3, 1.0, 0.8],
-                   [1.0, 1.2, 1.0, 1.1]];
+    let mut terrain = [[0.0; NCOLS]; NROWS];
+    let mut i = 0.;
+    let mut j = 0.;
+    for ri in 0..NROWS {
+        for ci in 0..NCOLS {
+            let z: f32 = i + j;
+            terrain[ri][ci] = z;
+            i += 0.1;
+        }
+        j += 0.1;
+    }
 
     // Water depth in meters at t0
-    let mut depth = [[0.05, 0.05, 0.05, 0.05],
-                     [0.05, 0.05, 0.05, 0.05], 
-                     [0.05, 0.05, 0.05, 0.05], 
-                     [0.05, 0.05, 0.05, 0.05]];
+    let mut depth = [[0.0; NCOLS]; NROWS];
+    for ri in 0..NROWS {
+        for ci in 0..NCOLS {
+            depth[ri][ci] = depth_0;
+        }
+    }
 
-    let mut velocity = [[[0., 0.], [0., 0.], [0., 0.]],
-                        [[0., 0.], [0., 0.], [0., 0.]],
-                        [[0., 0.], [0., 0.], [0., 0.]]];
+    // Initial velocity vectors
+    let mut velocity = [[[0.0; 2]; NCOLS]; NROWS];
+    for ri in 0..NROWS {
+        for ci in 0..NCOLS {
+            velocity[ri][ci][0] = 0.;
+            velocity[ri][ci][1] = 0.;
+        }
+    }
     
-    // Roughness values 
-    let kst = [[10., 10., 10., 10.],
-               [10., 10., 10., 10.],
-               [10., 10., 10., 10.],
-               [10., 10., 10., 10.]];
-    
+    // Default roughness values 
+    let mut kst = [[0.0; NCOLS]; NROWS];
+    for ri in 0..NROWS {
+        for ci in 0..NCOLS {
+            kst[ri][ci] = kst_default;
+        }
+    }
+       
     // Calculate initial water levels
     let mut water_level = [[0.0; NCOLS]; NROWS];
     for ri in 0..NROWS {
@@ -43,31 +62,24 @@ fn main() {
     let mut energy = [[0.0; NCOLS]; NROWS];
     for ri in 0..NROWS {
         for ci in 0..NCOLS {
-            energy[ri][ci] = bernoulli(terrain[ri][ci],
-                                       depth[ri][ci],
-                                       velocity[ri][ci],
-                                       9.81);
+            energy[ri][ci] = bernoulli(&terrain[ri][ci],
+                                       &depth[ri][ci],
+                                       &velocity[ri][ci],
+                                       &G);
         }
     }
 
-
-
+    // -------------------------------------------------------------------------
+    // Run test simulation
     let mut dt_now = 0.0;
     while dt_now <= DT_END {
-        for ri in 0..NROWS {
-            for ci in 0..NCOLS {
+        for ri in 1..(NROWS-1) {
+            for ci in 1..(NCOLS-1) {
                 let mut local_energy = [[0.0; 3]; 3];
 
-                for i in ri-1..ri+1 {
-                    for j in ci-1..ci+1 {
-                        if ri < 0 || ri >= NROWS || ci < 0 || ci >= NCOLS {
-                            local_energy[i][j] = 0.0; 
-                        } else {
-                            local_energy[i][j] = bernoulli(&terrain[i][j], 
-                                                           &depth[i][j],
-                                                           &velocity[i][j],
-                                                           &G);
-                        }
+                for i in 0..3 {
+                    for j in 0..3 {
+                        local_energy[i][j] = energy[ri+i-1][ci+j-1];
                     }
                 }
 
